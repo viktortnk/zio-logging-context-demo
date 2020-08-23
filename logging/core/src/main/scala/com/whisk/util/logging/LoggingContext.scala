d88152bbe911955b23a860efe50b590692efe1ba
Viktor Taranenko <viktortnk@gmail.com>
2020-08-23 10:19:34 +0100

    making logging covariant

--- logging/core/src/main/scala/com/whisk/util/logging/LoggingContext.scala
+++ logging/core/src/main/scala/com/whisk/util/logging/LoggingContext.scala
@@ -4,7 +4,7 @@ import cats.effect.Bracket
 import cats.syntax.flatMap._
 import cats.syntax.functor._
 
-trait LoggingContext[F[_]] {
+trait LoggingContext[+F[_]] {
   type Entry = Map[String, String]
 
   def get: F[Entry]
@@ -15,9 +15,11 @@ trait LoggingContext[F[_]] {
 
   def update(f: Entry => Entry): F[Unit]
 
-  def withLocal[A, E](value: Map[String, String])(use: F[A])(implicit B: Bracket[F, E]): F[A] =
+  def withLocal[F1[X] >: F[X], A, E](
+      value: Map[String, String]
+  )(use: F1[A])(implicit B: Bracket[F1, E]): F1[A] =
     for {
-      oldValue <- get
+      oldValue <- get.asInstanceOf[F1[Entry]]
       b <- B.bracket(set(oldValue ++ value))(_ => use)(_ => set(oldValue))
     } yield b
 }
